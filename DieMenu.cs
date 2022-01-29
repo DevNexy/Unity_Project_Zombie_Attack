e0844a6334c96b77e190ce33fc2956386f3f82ce
devjisong <92451281+devjisong@users.noreply.github.com>
2022-01-29 22:26:46 +0900

    DieMenu

---
+++ DieMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DieMenu : MonoBehaviour
+{
+    public GameObject dieMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerHp.Hp == 0.0f)
+        {
+
+            Time.timeScale = 0f;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            dieMenu.SetActive(true);
+
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            /*Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;*/
+            dieMenu.SetActive(false);
+        }
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+        PlayerHp.Hp = 100.0f;
+        BulletScript.attackNum = 0;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        dieMenu.SetActive(false);
+    }
+    public void GameExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+
+#endif
+        Debug.Log("종료");
+    }
+}
