8a4d104ea9f7e2869eda5a2fced46b01d6eacdce
devjisong <92451281+devjisong@users.noreply.github.com>
2022-01-29 22:28:30 +0900

    Item_Pick

---
+++ Item_Pick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Pick : MonoBehaviour
+{
+    public static int count = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("플레이어와 닿았습니다.");
+            Destroy(this.gameObject);
+            count++;
+            Debug.Log("아이템을 주웠습니다." + count);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
