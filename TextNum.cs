993df073e776225fa8918c5690de9565f792c3a9
devjisong <92451281+devjisong@users.noreply.github.com>
2022-01-29 22:30:47 +0900

    TextNum

---
+++ TextNum.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextNum : MonoBehaviour
+{
+    public Text KillNum;
+    public Text FoodNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FoodNum.text = Item_Pick.count.ToString();
+        KillNum.text = BulletScript.attackNum.ToString();
+    }
+}
