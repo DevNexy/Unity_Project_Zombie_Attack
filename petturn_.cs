f45596e31e5088609a3b72c1d151d41fee2375ab
devjisong <92451281+devjisong@users.noreply.github.com>
2022-01-29 22:34:41 +0900

    petturn
    Menu pattern script.

---
+++ petturn_.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class petturn_ : MonoBehaviour
+{
+    public float timer;
+    public GameObject petturn;
+    public float speed = 70;
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        this.transform.position += Vector3.right * (speed * Time.deltaTime);
+    }
+}
