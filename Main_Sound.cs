ea1080cb274e05b1f75f7bb3a1446d6e3802e62d
devjisong <92451281+devjisong@users.noreply.github.com>
2022-01-29 22:33:20 +0900

    Main_Sound
    Script for background music on the main screen.

---
+++ Main_Sound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string soundName;
+    public AudioClip clip;
+}
+
+public class Main_Sound : MonoBehaviour
+{
+    [Header("사운드 등록")]
+    [SerializeField] Sound bgmSounds;
+
+    [Header("브금 플레이어")]
+    [SerializeField] AudioSource bgmPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayBGM();
+    }
+    
+    public void PlayBGM()
+    {
+        bgmPlayer.clip = bgmSounds.clip;
+        bgmPlayer.Play();
+    }
+}
