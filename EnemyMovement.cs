5e0b08e423cf37b6155e75ec232322c0709bcad7
devjisong <92451281+devjisong@users.noreply.github.com>
2022-01-29 22:27:17 +0900

    EnemyMovement

---
+++ EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Transform player;
+
+    private NavMeshAgent nav;
+
+    void Awake()
+    {
+        //Player 태그로 설정된 오브젝트의 트랜스폼 값을 가져옴
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //SetDestination(목적지) => 길찾기의 목적지를 설정
+        nav.SetDestination(player.position);
+    }
+}
