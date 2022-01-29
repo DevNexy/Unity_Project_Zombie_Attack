b569acdd52c6242f89fc3c6fa2116dd628f4ba3f
devjisong <92451281+devjisong@users.noreply.github.com>
2022-01-29 22:36:26 +0900

    Zombie_petturn

---
+++ Zombie_petturn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie_petturn : MonoBehaviour
+{
+    public GameObject image_0;
+    public GameObject image_1;
+    public float timer;
+
+
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 0)
+        {
+            image_1.SetActive(true);
+            image_0.SetActive(false);
+        }
+    }
+}
